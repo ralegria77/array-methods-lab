89de52a2e89a4aca3d2c987cb2620c456cb9346a
rebec <ralegria77@gmail.com>
2020-11-29 23:34:19 -0800

    commit tree b5e59b4d510e6559d9b7148891ae96c7ed9cb11b

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,26 +3,65 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.sort 
+    array.sort
 end
 
 def using_reverse(array)
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]  
-    array.reverse 
+    array.reverse
 end
 
 def using_first(array)
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
     array.first
 end
 
 def using_last(array)
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.last 
+    array.last
 end
 
 def using_size(array)
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.size 
+    array.length
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def using_include(array, element)
+#     array.include?(element)
+# end
+
+# def using_sort(array)
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     array.sort 
+# end
+
+# def using_reverse(array)
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]  
+#     array.reverse 
+# end
+
+# def using_first(array)
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     array.first
+# end
+
+# def using_last(array)
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     array.last 
+# end
+
+# def using_size(array)
+#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
+#     array.size 
+# end
